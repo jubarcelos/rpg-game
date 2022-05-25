933aed1d64a3d40a6dff1a221b2d9b1c6c76ca77
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-05-25 11:18:33 -0300

    Feat: Mage class
    Extended by Archetype.

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,35 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static _instancesCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get special(): number {
+    return this.special;
+  }
+
+  get cost(): number {
+    return this.cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._instancesCount += 1;
+    return this._instancesCount;
+  }
+  
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Mage;
