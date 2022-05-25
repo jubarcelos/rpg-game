ca3310a27a0b2b9c47c00c2fa0951c70075e8e82
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-05-25 16:15:06 -0300

    Fix: Fighter Interface
    Fix file organization to follow the brief.

--- tests/sources/index.OCP.ts
+++ tests/sources/index.OCP.ts
@@ -1,4 +1,4 @@
-import { runBattles, pve, pvp, player1, player2, player3, monster1, monster2 } from '../../src';
+import { runBattles, pve, pvp, player1, player2, player3, monster1, monster2 } from '../../src/Fighter';
 import Battle from '../../src/Battle';
 import Fighter from '../../src/Fighter';
 
--- tests/sources/index.pve.ts
+++ tests/sources/index.pve.ts
@@ -1,4 +1,4 @@
-import { pve } from '../../src';
+import { pve } from '../../src/Fighter';
 import { PVE } from '../../src/Battle';
 
 const func = (p: PVE) => { };
