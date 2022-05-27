eb8898a67881984046a5705ca46d036b0ece856c
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-05-27 16:01:17 -0300

    Feat: PVP
    Class what provide a combat between two Characters type Fighter

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,24 @@
+// import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _player2: Fighter;
+
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this._player2 = player2;
+    this.combat();
+  }
+
+  combat(): void {
+    while (this.player.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this.player.attack(this._player2);
+      this._player2.attack(this.player);
+    }
+  }
+}
+
+export default PVP;
+
+// console.log(new PVP(new Character('player1'), new Character('player2')));
